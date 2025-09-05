f13dcb785b14ddea5600519297e9da138eaa212a
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-05 10:25:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist'
-  }
-})
+module.exports = defineConfig({
+  plugins: [react()]
+});
