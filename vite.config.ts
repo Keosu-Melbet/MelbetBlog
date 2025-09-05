180c42d6e6b93a95cf0e072399316e4bfbf677d6
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-05 18:25:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'client'),
   plugins: [react()],
-  server: {
-    port: 3000
-  },
   build: {
-    outDir: 'dist'
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client/src')
+    }
   }
 });
